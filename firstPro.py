e125651002c3488de6ec6c2300f5d22a6432e2c1
yangzitian <1255847519@qq.com>
2018-04-04 17:05:49 +0800

    第一次上传flask

---
+++ firstPro.py
@@ -0,0 +1,20 @@
+#从flask框架导入Flask类
+from flask import Flask
+
+# 初始化一个Flask对象
+# Flask（）
+#  需要传入一个参数__name__
+# 1.方便flask框架去寻找资源
+# 2.方便flask插件比如Flask-Sqlalchemy出现错误的时候，好去寻找问题所在的位置
+app = Flask(__name__)
+
+# @app.route是一个装饰器，@开头，并且在函数的上面，说明是装饰器
+# 这个装饰器的作用，是做一个url与视图函数的映射
+# 127.0.0.1:5000/ --> 去请求hello_world这个函数，然后将结果返回给浏览器
+@app.route('/')
+def hello_world():
+    return '我是第一个flask程序!'
+
+# 如果当前这个文件是作为入口程序运行，那么就执行app.run()
+if __name__ == '__main__':
+    app.run()
